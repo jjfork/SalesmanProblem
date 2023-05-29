c1a7bdb2a887035d0720b602eb9c3b1388a33c57
jjfork <jakubiakrjulian@gmail.com>
2023-05-29 17:28:22 +0200

    added method to generate data

---
+++ HelpingMethods/table_generator.py
@@ -0,0 +1,28 @@
+import random
+
+
+def table_generator(number_of_cities, min_d, max_d):
+    """
+    Generates a table for Traveling Salesman Problem algorithm
+
+    :param number_of_cities: -> int, number of cities
+    :param min_d: minimum distance between cities
+    :param max_d: maximum distance between cities
+    :return: table as a list of list
+
+    - Distances between cities are random
+    - Distances are symmetric, [i][j] = [j][i]
+    """
+    table = []
+    for i in range(number_of_cities):
+        value = []
+        for j in range(number_of_cities):
+            if i == j:
+                value.append(0)
+            elif j > i:
+                value.append(random.randint(min_d, max_d))
+            else:
+                value.append(table[j][i])
+        table.append(value)
+    print(table)
+    return table
--- main.py
+++ main.py
@@ -1,15 +1,10 @@
+from SalesmanProblem.HelpingMethods.table_generator import table_generator
 from SalesmanProblem.TravelingSalesmanProblem.helda_karpa import held_karp
 from SalesmanProblem.TravelingSalesmanProblem.naiwna import travelling_salesman_problem
 
 if __name__ == "__main__":
     starting_point = 0
-    example_data = [
-    [0, 10, 15, 20, 25],
-    [10, 0, 35, 40, 45],
-    [15, 35, 0, 30, 20],
-    [20, 40, 30, 0, 10],
-    [25, 45, 20, 10, 0]
-]
+    example_data = table_generator(10, 1, 9)
 
     print("rozwiązanie naiwne:")
     print(travelling_salesman_problem(example_data, starting_point))
